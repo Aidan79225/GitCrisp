f2e2fca35238f797fe608243e1f213204ec112a0
Aidan Wang <aidan79225@gmail.com>
2026-04-09 00:48:29 +0800

    feat(domain): add LocalBranchInfo entity

---
+++ tests/domain/test_entities_local_branch_info.py
@@ -0,0 +1,18 @@
+from git_gui.domain.entities import LocalBranchInfo
+
+
+def test_local_branch_info_fields():
+    b = LocalBranchInfo(
+        name="master", upstream="origin/master",
+        last_commit_sha="a1b2c3d", last_commit_message="fix: x",
+    )
+    assert b.name == "master"
+    assert b.upstream == "origin/master"
+    assert b.last_commit_sha == "a1b2c3d"
+    assert b.last_commit_message == "fix: x"
+
+
+def test_local_branch_info_upstream_optional():
+    b = LocalBranchInfo(name="wip", upstream=None,
+                        last_commit_sha="abc", last_commit_message="WIP")
+    assert b.upstream is None
